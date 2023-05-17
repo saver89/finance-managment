e1c2e58638cc2fbe96e00ae1f9fc873649f3c825
vladimir.nee <neelkim1989@gmail.com>
2023-05-17 10:03:15 +0500

    feat: add config

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
+	"github.com/saver89/finance-management/config"
 	db "github.com/saver89/finance-management/internal/repository/postgres/sqlc"
 	"github.com/saver89/finance-management/internal/server"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/finances?sslmode=disable"
-	serverAddress = "0.0.0.0:8099"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := config.LoadConfig("./config")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+	fmt.Printf("config: %+v\n", config)
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := server.NewServer(store)
 
-	err = server.Run(serverAddress)
+	err = server.Run(config.ServerAddress)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
